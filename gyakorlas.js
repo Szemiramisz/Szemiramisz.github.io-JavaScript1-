bd1b9856de623ddeb18fbfedbf5ff696cd9c6df1
szokeszemi <92374201+Szemiramisz@users.noreply.github.com>
2022-04-05 16:46:42 +0200

    gyakorlas.js has been added

---
+++ gyakorlas.js
@@ -0,0 +1,31 @@
+let foo= 42; //deklarálj, változó létrehozása. A foo a változó azonosítója, neve.
+let bar=55; //inicializálj, inicializáltuk a bar  nevű változót
+
+
+// változók értékeinek felcserélése
+
+let dri = 42;
+foo=bar;
+bar=dri;
+
+// 55
+
+let a= 20;
+let b = 35;
+console.log(a+b);
+
+//"55"
+let c = "5";
+let d ="5";
+console.log(c+d);
+
+//undefined
+let e; // Az e változó inicializálatlan, ezért az értéke undefined
+
+// "hello world"
+let f="hello";
+let g="";
+let h="word";
+console.log(f + g + h);  
+
+
